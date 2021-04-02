27f3bfc01397b38b21989a8e902b57a58d3c18b9
fanolj <fanoljakupi@gmail.com>
2021-04-02 14:47:35 +0200

    Nise qet projekt

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur fuga sint accusantium maiores deserunt laudantium beatae explicabo, doloremque aperiam. Dolore molestias exercitationem provident aperiam modi suscipit beatae consequatur minus libero nesciunt quidem labore numquam quo, minima officiis, et architecto? Dolore ad quos autem earum non beatae perspiciatis illo architecto quod, nihil placeat facilis ea optio? Aut ex voluptatum dignissimos ut sit pariatur provident asperiores. Id quod velit quisquam pariatur odio accusantium similique error inventore deleniti dolorem dolorum perspiciatis, facilis maxime adipisci nam vel repellendus optio alias quia tempore voluptatibus dicta dolor repudiandae magnam? Eveniet possimus totam sequi eos laudantium id.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur fuga sint accusantium maiores deserunt laudantium beatae explicabo, doloremque aperiam. Dolore molestias exercitationem provident aperiam modi suscipit beatae consequatur minus libero nesciunt quidem labore numquam quo, minima officiis, et architecto? Dolore ad quos autem earum non beatae perspiciatis illo architecto quod, nihil placeat facilis ea optio? Aut ex voluptatum dignissimos ut sit pariatur provident asperiores. Id quod velit quisquam pariatur odio accusantium similique error inventore deleniti dolorem dolorum perspiciatis, facilis maxime adipisci nam vel repellendus optio alias quia tempore voluptatibus dicta dolor repudiandae magnam? Eveniet possimus totam sequi eos laudantium id.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {  }
+}
+</script>
